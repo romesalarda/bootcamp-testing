dc333d7c2c626af23fa38a01965d601c91bf853d
Rome Salarda <romxsalarda45@gmail.com>
2026-03-24 10:15:34 +0000

    new tests added

--- mymath/factorial.py
+++ mymath/factorial.py
@@ -5,6 +5,9 @@ def factorial(n):
     :param int n: The factorial to calculate
     :return: The resultant factorial
     """
+    if n < 0:
+        raise ValueError("Factorial is not defined for negative numbers.")
+    
     factorial = 1
     for i in range(1, n + 1):
         factorial = factorial * i
--- tests/test_factorial.py
+++ tests/test_factorial.py
@@ -1,6 +1,21 @@
 import pytest
 from mymath.factorial import factorial
 
-
 def test_3():
+
+    
     assert factorial(3) == 6
+
+def test_5():
+    assert factorial(5) == 120
+
+def test_4():
+    assert factorial(4) == 24
+
+def test_42():
+    assert factorial(42) == 1405006117752879898543142606244511569936384000000000
+
+def test_negative():
+    with pytest.raises(ValueError) as excinfo:
+        factorial(-1)
+        
